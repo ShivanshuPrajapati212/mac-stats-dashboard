20fd6ab470cfef43c5de95ed71f128bdd6344d4d
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-02-24 13:39:29 +0530

    something right

--- server/internal/ws/ws.go
+++ server/internal/ws/ws.go
@@ -1,17 +1,27 @@
 package ws
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true // allow all origins (safe for local dev)
 	},
+}
+
+type Message struct {
+	CPU      float64 `json:"cpu"`
+	TotalRAM float64 `json:"total_ram"`
+	UsedRAM  float64 `json:"used_ram"`
 }
 
 func WsHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,9 +33,12 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 
 	for {
-		message := time.Now().Format("15:04:05")
+		cpuPercent, _ := cpu.Percent(0, false)
+		memStats, _ := mem.VirtualMemory()
 
-		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		stats := Message{CPU: cpuPercent[0], TotalRAM: float64(memStats.Total), UsedRAM: float64(memStats.Used)}
+
+		err = conn.WriteJSON(stats)
 		if err != nil {
 			log.Println("Write error:", err)
 			break
@@ -33,4 +46,20 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 
 		time.Sleep(1 * time.Second)
 	}
+}
+
+func getProcessByName(name string) (*process.Process, error) {
+	processes, err := process.Processes()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, p := range processes {
+		n, _ := p.Name()
+		if n == name {
+			return p, nil
+		}
+	}
+
+	return nil, fmt.Errorf("process not found")
 }
